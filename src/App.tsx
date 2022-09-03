f797e5b9e0b5ffb8912e48131d8483bb9d5e345a
Thang Nguyen Vu <nguyenvuthang942008@gmail.com>
2022-09-03 22:31:19 +0700

    initialize frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main">
+      <div className="wrapper">
+        <h1>Todos: </h1>
+        <div className="todo-list">
+          <ul>
+            <li>
+              <span>
+                <input type="checkbox" />
+                clean the room
+              </span>
+              <span>remove</span>
+            </li>
+            <li>
+              <span>
+                <input type="checkbox" />
+                order stuffs from shopee
+              </span>
+              <span>remove</span>
+            </li>
+            <li>
+              <span>
+                <input type="checkbox" />
+                make a call
+              </span>
+              <span>remove</span>
+            </li>
+            <li>
+              <span>
+                <input type="checkbox" />
+                have lunch
+              </span>
+              <span>remove</span>
+            </li>
+          </ul>
+        </div>
+        <div className="add-todos">
+          <input type="text" name="" id="" />
+          <button className="add">Add more</button>
+        </div>
+      </div>
+    </main>
   );
 }
 
